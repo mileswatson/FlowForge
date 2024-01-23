ac7bf10721ab309fc67ce6a257dbd4f882d98cf3
Miles Watson <contact@mileswatson.net>
2024-01-23 21:31:31 +0000

    Added dydx test.

--- flowforge_ns2/src/lib.rs
+++ flowforge_ns2/src/lib.rs
@@ -1,7 +1,15 @@
 use flowforge::quantities::Float;
 
+use dfdx::prelude::*;
+
 #[no_mangle]
 pub extern "C" fn rust_function() -> Float {
+    let device = Cpu::default();
+
+    let x = device.tensor([[1., 0.], [0., 1.]]).matmul(device.tensor([[0., -1.], [1.,0.]]));
+
+    println!("{:?}", x.array());
+
     println!("called!");
     let x: Float = 5.;
     x
