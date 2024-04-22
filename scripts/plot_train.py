a75f335c20e857910bcb5710e4c103a3065950dd
Miles Watson <contact@mileswatson.net>
2024-04-22 15:42:42 +0100

    Removed first sample from plot.

--- scripts/plot_train.py
+++ scripts/plot_train.py
@@ -10,10 +10,10 @@ args = parser.parse_args()
 with open(args.input, "r") as f:
     input = json.load(f)
 
-timestamps = np.array(input["timestamps"])
-utility = np.array(input["utility"])
-bandwidth = np.array(input["bandwidth"])
-rtt = np.array(input["rtt"])
+timestamps = np.array(input["timestamps"][1:])
+utility = np.array(input["utility"][1:])
+bandwidth = np.array(input["bandwidth"][1:])
+rtt = np.array(input["rtt"][1:])
 
 # plot
 fig, axes = plt.subplots(4)
